a47c79f0af84d38dd1d50114a455b45dbbcec67a
AjeethP <ajeeth.prabaher@aenetworks.com>
2022-10-30 11:09:59 -0400

    updates for test module

--- s3/output.tf
+++ s3/output.tf
@@ -1,11 +1,11 @@
 output "s3_app_bucket_name" {
-  value = aws_s3_bucket.source_s3_bucket.bucket
+  value = aws_s3_bucket.source_s3_bucket[0].bucket
 }
 
 output "s3_app_bucket_arn" {
-  value = aws_s3_bucket.source_s3_bucket.arn
+  value = aws_s3_bucket.source_s3_bucket[0].arn
 }
 
 output "s3_app_bucket_id" {
-  value = aws_s3_bucket.source_s3_bucket.id
+  value = aws_s3_bucket.source_s3_bucket[0].id
 }
